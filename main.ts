240cf025f347bcd18654dfce23e7279c9e505115
dhruvathejas <dhruvathejasb@gmail.com>
2024-12-02 17:37:22 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Dhruva")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Sri")
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("BV")
+})
