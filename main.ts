4581b520c3ef03135291823d07f90db32a81c501
dhruvathejas <dhruvathejasb@gmail.com>
2024-12-04 17:33:09 +0530

    Reorganizing the buttons

--- main.ts
+++ main.ts
@@ -1,16 +1,21 @@
 /**
- * a-Me
+ * a-My father
  * 
  * b-My mother
  * 
- * c-My father
+ * a+b-Me
+ * 
+ * logo-My Family
  */
 input.onButtonPressed(Button.A, function () {
+    basic.showString("BV")
+})
+input.onButtonPressed(Button.AB, function () {
     basic.showString("Dhruva")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("Sri")
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showString("BV")
+    basic.showString("My Family")
 })
